1f28c033c6a5aa832ba8bc3b33178a686091201b
Bianca-14 <biancatecar14@gmail.com>
2024-11-09 16:04:00 +0200

    Add project files.

---
+++ Data/Tecar_Bianca_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tecar_Bianca_Lab2.Models;
+
+namespace Tecar_Bianca_Lab2.Data
+{
+    public class Tecar_Bianca_Lab2Context : DbContext
+    {
+        public Tecar_Bianca_Lab2Context (DbContextOptions<Tecar_Bianca_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tecar_Bianca_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Tecar_Bianca_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Tecar_Bianca_Lab2.Data;
+using Tecar_Bianca_Lab2.Models;
+
+namespace Tecar_Bianca_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Tecar_Bianca_Lab2.Data.Tecar_Bianca_Lab2Context _context;
+
+        public IndexModel(Tecar_Bianca_Lab2.Data.Tecar_Bianca_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            // Include the Publisher information when fetching Books
+            Book = await _context.Book
+                .Include(b => b.Publisher)  // This will include the Publisher data related to each Book
+                .ToListAsync();
+        }
+    }
+}
